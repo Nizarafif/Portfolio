c6e4dc3cfacbe7c522aa5c67654e3398ce7af579
Nizarafif <nizarkenzi50@gmail.com>
2026-06-29 23:01:49 +0700

    fix: update eslint config extension references to include .js suffix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals.js", "next/typescript.js"),
   {
     ignores: [
       ".next/**",
